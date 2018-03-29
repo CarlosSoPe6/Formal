3228a939680897324b7139b9cb94c27587936245
Carlos Soto Pérez <carlos348@outlook.com>
2018-03-29 17:35:50 -0600

    Second commit

---
+++ src/MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+import analyzers.GrammarAnalyzer;
+
+public class MainFrame {
+	public static void main(String args[]) {
+		JFrame frame = new GrammarAnalyzer();
+	}
+}
---
+++ src/analyzers/GrammarAnalyzer.java
@@ -0,0 +1,7 @@
+package analyzers;
+
+import javax.swing.JFrame;
+
+public class GrammarAnalyzer extends JFrame{
+	
+}
