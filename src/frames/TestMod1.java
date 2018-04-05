062f6f67cf232c9c1b8e0279d671aa65dd6eae32
Alejandro Rios <jassoalex97@hotmail.com>
2018-04-05 08:57:33 -0500

    Linea corregida en Test

--- src/frames/TestMod1.java
+++ src/frames/TestMod1.java
@@ -8,7 +8,6 @@ public class TestMod1 {
 
 	public static void main(String[] args) {
 		String string = JOptionPane.showInputDialog("Ingresa una cadena: ");
-		new ReversePolishNotation(string);
 		JOptionPane.showMessageDialog(null, new ReversePolishNotation(string).getPostfix());
 	}
 
