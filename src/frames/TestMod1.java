1fac21401d7682842f38853dc8aa06382f35413d
Alejandro Rios <jassoalex97@hotmail.com>
2018-04-12 16:27:31 -0500

    Actualización del test para prueba de las matrices y clase para conversión de las matrices de AFNe a AFN.

--- src/frames/TestMod1.java
+++ src/frames/TestMod1.java
@@ -1,19 +1,66 @@
 package frames;
 
+import java.util.LinkedList;
+
 import javax.swing.JOptionPane;
 
+import modules.NFDeToNFD;
 import modules.RPNToNFDE;
 import modules.ReversePolishNotation;
 
 public class TestMod1 {
 
+	@SuppressWarnings({ "unused", "unchecked" })
 	public static void main(String[] args) {
-		String string = JOptionPane.showInputDialog("Ingresa una cadena: ");
-		String rpn = new ReversePolishNotation(string).getPostfix();
-		JOptionPane.showMessageDialog(null, rpn);
+//		String string = JOptionPane.showInputDialog("Ingresa una cadena: ");
+//		String rpn = new ReversePolishNotation(string).getPostfix();
+//		JOptionPane.showMessageDialog(null, rpn);
+//		
+//		RPNToNFDE converter = new RPNToNFDE();
+//		converter.convert(rpn);
+//		
+		LinkedList<Integer>[][] matrizConEpsilon = new LinkedList[5][255]; // [numero de estados][simbolos posibles]
+		for(int i = 0; i < 5; i ++) {
+			for(int j = 0; j < 255; j ++) {
+				matrizConEpsilon[i][j] = new LinkedList<Integer>();
+			}
+		}
 		
-		RPNToNFDE converter = new RPNToNFDE();
-		converter.convert(rpn);
+		// Automata hardcodeado
+		matrizConEpsilon[0][254].add(1);
+		matrizConEpsilon[1]['0' - ' '].add(1);
+		matrizConEpsilon[1]['1' - ' '].add(1);
+		matrizConEpsilon[1]['1' - ' '].add(2);
+		matrizConEpsilon[2]['0' - ' '].add(3);
+		matrizConEpsilon[2]['1' - ' '].add(3);
+		matrizConEpsilon[3]['0' - ' '].add(4);
+		matrizConEpsilon[3]['1' - ' '].add(4);
+		matrizConEpsilon[4][254].add(1);
+		
+		//Prints hardcodeados
+		for(int i = 0; i < 5; i ++) {
+			for(int j = 0; j < 255; j ++) {
+				if(!matrizConEpsilon[i][j].isEmpty()) System.out.print(matrizConEpsilon[i][j].toString() + j + " ");
+			}
+			System.out.println();
+		}
+		
+		//Llamadas a la funcion de conversión
+		LinkedList<Integer>[][] matrizSinEpsilon = new LinkedList[5][255];
+		NFDeToNFD converter2 = new NFDeToNFD(matrizConEpsilon);
+		matrizSinEpsilon = converter2.upperDeltaFunc();
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("Matriz de transición sin epsilon trnasiciones!");
+		
+		for(int i = 0; i < 5; i ++) {
+			for(int j = 0; j < 254; j ++) {
+				if(!matrizSinEpsilon[i][j].isEmpty()) System.out.print(matrizSinEpsilon[i][j].toString() + j + " ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
--- src/modules/NFDeToNFD.java
+++ src/modules/NFDeToNFD.java
@@ -4,12 +4,60 @@ import java.util.LinkedList;
 
 public class NFDeToNFD {
 	
-	private LinkedList<Character>[][] transitionMatrix = null;
+	private static final char EPSILON = 254;
+	private LinkedList<Integer>[][] transitionMatrix = null;
 	
-	public NFDeToNFD(LinkedList<Character>[][] tMatrix) {
+	public NFDeToNFD(LinkedList<Integer>[][] tMatrix) {
 		this.transitionMatrix = tMatrix;
 	}
 	
 	// Algoritmos para hacer la conversión de la matriz
+	
+	@SuppressWarnings("unchecked")
+	public LinkedList<Integer> eTransition(LinkedList<Integer> list) {
+		LinkedList<Integer> statesSet = (LinkedList<Integer>) list.clone();
+		for(int i = 0; i < list.size(); i++) {
+			int j = 0;
+			while(j < this.transitionMatrix[list.get(i)][NFDeToNFD.EPSILON].size()) {
+				if(!statesSet.contains(this.transitionMatrix[list.get(i)][NFDeToNFD.EPSILON].get(j))) {
+					statesSet.add(this.transitionMatrix[list.get(i)][NFDeToNFD.EPSILON].get(j));
+					j ++;
+				}
+			}
+		}
+		return statesSet;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public LinkedList<Integer> lowerDeltaFunc(LinkedList<Integer> list, char c) {
+		LinkedList<Integer> statesSet = new LinkedList<Integer>();
+		for(int i = 0; i < list.size(); i++) {
+			if(statesSet.isEmpty())
+				statesSet = (LinkedList<Integer>) this.transitionMatrix[list.get(i)][c].clone();
+			else {
+				int j = 0;
+				while(j < this.transitionMatrix[list.get(i)][c].size()) {
+					if(!statesSet.contains(this.transitionMatrix[list.get(i)][c].get(j))) {
+						statesSet.add(this.transitionMatrix[list.get(i)][c].get(j));
+						j ++;
+					}
+				}
+			}
+		}
+		return statesSet;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public LinkedList<Integer>[][] upperDeltaFunc() {
+		LinkedList<Integer>[][] deltaMatrix = new LinkedList[this.transitionMatrix.length][254];
+		for(int i = 0; i < this.transitionMatrix.length; i++) {
+			for(int j = 0; j < 254; j ++) {
+				LinkedList<Integer> currentState = new LinkedList<>();
+				currentState.add(i);
+				deltaMatrix[i][j] = eTransition(lowerDeltaFunc(eTransition(currentState), (char) j));
+			}
+		}
+		return deltaMatrix;
+	}
 	
 }
